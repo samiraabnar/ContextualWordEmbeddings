20fc58621e79886c2cd5b6315df97237865adc8c
Samira Abnar <samiraabnar@gmail.com>
2017-12-12 11:52:55 +0100

    batch size

--- src/LSTMLanguageModel.py
+++ src/LSTMLanguageModel.py
@@ -229,7 +229,7 @@ if __name__ == '__main__':
     hparams.DEFINE_integer("training_size", 100000, "total number of training samples")
     hparams.DEFINE_integer("number_of_epochs", 100, "Epoch to train [25]")
     hparams.DEFINE_integer("vocab_size", 10000, "The size of vocabulary [10000]")
-    hparams.DEFINE_integer("batch_size", 32, "The size of batch images [32]")
+    hparams.DEFINE_integer("batch_size", 1, "The size of batch images [32]")
     hparams.DEFINE_integer("depth", 2, "Depth [1]")
     hparams.DEFINE_integer("max_nsteps", 1000, "Max number of steps [1000]")
     hparams.DEFINE_integer("number_of_hidden_units", 512, "The size of hidden layers")
